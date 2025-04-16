790c074f2d792ad47fea137ca1dac6190de02b26
Duplo <Blitzcolt34@gmail.com>
2025-04-16 23:42:23 +0200

    first test

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -1,13 +1,38 @@
-import { Component } from "@angular/core";
-import { RouterOutlet } from "@angular/router";
+import { RouterOutlet } from '@angular/router';
+import { ActivatedRoute, Router, NavigationEnd } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { filter, switchMap } from 'rxjs/operators';
 
 @Component({
-  selector: "app-root",
+  selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
-  templateUrl: "./app.component.html",
-  styleUrl: "./app.component.css",
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = "TINF24B5";
+  title = 'Angis Website';
+  urlName: string | null = '';
+  constructor(private router: Router, private route: ActivatedRoute) {}
+  ngOnInit(): void {
+    // Auf jede Navigation hören
+    this.router.events
+      .pipe(filter((event) => event instanceof NavigationEnd))
+      .subscribe(() => {
+        const currentUrl = this.router.url;
+        const segments = currentUrl.split('/');
+        const secondSegement = segments[1];
+        switch (secondSegement) {
+          case 'compliments':
+            this.urlName = 'Komplimente';
+            break;
+          case 'hallo':
+            this.urlName = 'Hallo';
+            break;
+          default:
+            this.urlName = 'Home';
+            break;
+        }
+      });
+  }
 }
--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { ComplimentsComponent } from './compliments/compliments.component';
+import { HalloComponent } from './hallo/hallo.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'compliments', component: ComplimentsComponent },
+  { path: 'hallo', component: HalloComponent },
+];
